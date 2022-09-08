e43c8c579a9c8bb98a79aeebf9ec6f26c1c2b30f
Arman <armanist84@mail.ru>
2022-09-08 19:37:17 +0400

    Adding AlertMessage component

---
+++ src/components/AlertMessage.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+	props: {
+		message: {
+			type: String,
+			required: true
+		},
+		type: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
+<template>
+	<div :class="['material-alert left-align', type]">
+		{{ message }}
+	</div>
+</template>
